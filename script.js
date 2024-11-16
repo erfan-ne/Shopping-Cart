a272d0b7e4a05ca9fe08b5d4f02ea1db01179280
erfanne75 <erfan.neshati@gmail.com>
2024-11-16 13:18:45 +0330

    project compeleted

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var age = prompt("How old are you?")
-var gender = prompt("What is your gender?")
+var gender = prompt("What is your gender? male or female")
 
+if (age < 18 || gender == "female") {
+    alert("Not Ok")
+} else if (age >= 18 & gender == "male") {
+    alert ("ok")
+} else {
+    alert ("The information entered is not correct")
+}
