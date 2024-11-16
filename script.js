76cd68bf52b9d012942925d6fd90f69ef64dd4f1
erfanne75 <erfan.neshati@gmail.com>
2024-11-16 16:31:14 +0330

    ternary project

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var age = prompt("How old are you?")
 var gender = prompt("What is your gender? male or female")
 
-if (age < 18 || gender == "female") {
-    alert("Not Ok")
-} else if (age >= 18 & gender == "male") {
-    alert ("ok")
-} else {
-    alert ("The information entered is not correct")
-}
+// if (age < 18 || gender == "female") {
+//     alert("Not Ok")
+// } else if (age >= 18 & gender == "male") {
+//     alert ("ok")
+// } else {
+//     alert ("The information entered is not correct")
+// }
+
+age < 18 || gender == "female" ? alert("Not Ok") : (age >= 18 & gender == "male") ? alert ("ok") : alert ("The information entered is not correct")
